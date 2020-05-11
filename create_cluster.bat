da0c6f2a95df174548057891828019160e77b521
Ahmed Farghaly <ahmed.asfora@gmail.com>
2020-05-11 11:41:48 +0300

    fix cluster

--- create_cluster.bat
+++ create_cluster.bat
@@ -1,13 +1,15 @@
-echo "Create Cluster"
+@echo off
+
+echo Create Cluster
 eksctl create cluster ^
     --name DevamsK8sCluster ^
     --region us-west-2 ^
     --nodegroup-name standard-workers ^
     --node-type t3.micro ^
-    --node-ami auto ^
     --nodes 2 ^
     --nodes-min 1 ^
-    --nodes-max 4
+    --nodes-max 4 ^
+    --managed
 
-echo "Create a kubectl configuration file"
+echo Create a kubectl configuration file
 aws eks --region us-west-2 update-kubeconfig --name DevamsK8sCluster                     
--- create_cluster.sh
+++ create_cluster.sh
@@ -6,10 +6,10 @@ eksctl create cluster \
     --region us-west-2 \
     --nodegroup-name standard-workers \
     --node-type t3.micro \
-    --node-ami auto \
     --nodes 2 \
     --nodes-min 1 \
-    --nodes-max 4
+    --nodes-max 4 \
+    --managed
 
 echo "Create a kubectl configuration file"
 aws eks --region us-west-2 update-kubeconfig --name DevamsK8sCluster                     
